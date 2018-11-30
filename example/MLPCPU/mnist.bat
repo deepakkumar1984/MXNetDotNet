59f149b4e190803916402c58fd73a7d30957ec0b
Deepak Kumar <deepak_kumarb@live.com>
2018-11-30 15:19:56 +1030

    ....

--- example/MLPCPU/mnist.bat
+++ example/MLPCPU/mnist.bat
@@ -1,7 +1,7 @@
-python -m wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-python -m wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-python -m wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-python -m wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 python mnist.py
 del t10k-images-idx3-ubyte.gz
 del t10k-labels-idx1-ubyte.gz
